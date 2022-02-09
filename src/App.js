6149b5c87b762d5372289247ded6bdadfe72003e
Saumya <saumyasingh1508@gmail.com>
2022-02-09 19:44:44 +0530

    Remove Item Button

--- src/App.js
+++ src/App.js
@@ -1,62 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './components/Navbar.js';
-import ProductList from './components/ProductList.js';
-import Footer from './components/Footer';
-import React,{useState} from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar.js";
+import ProductList from "./components/ProductList.js";
+import Footer from "./components/Footer";
+import React, { useState } from "react";
 
 function App() {
-  const products=[
+  const products = [
     {
       name: "IPhone X Max ",
       price: 99999,
-      quantity:0,
+      quantity: 0,
     },
     {
       name: "Redmi Note 10 ",
       price: 9999,
-      quantity:0,
-    }
-  ]
-  let [productList , setProductList] = useState(products);
+      quantity: 0,
+    },
+  ];
+  let [productList, setProductList] = useState(products);
   let [totalAmount, setTotalAmount] = useState(0);
 
-  const incrementQuantity=(index)=>{
-    let newProductList=[...productList];
-    let newTotalAmount= totalAmount;
+  const incrementQuantity = (index) => {
+    let newProductList = [...productList];
+    let newTotalAmount = totalAmount;
     newProductList[index].quantity++;
     newTotalAmount += newProductList[index].price;
     setTotalAmount(newTotalAmount);
     setProductList(newProductList);
-  }
-  const decrementQuantity=(index)=>{
-    let newProductList=[...productList];
-    let newTotalAmount= totalAmount;
-   if(newProductList[index].quantity >0)
-   {
-    newProductList[index].quantity--;
-    newTotalAmount -= newProductList[index].price;
-   }
+  };
+  const decrementQuantity = (index) => {
+    let newProductList = [...productList];
+    let newTotalAmount = totalAmount;
+    if (newProductList[index].quantity > 0) {
+      newProductList[index].quantity--;
+      newTotalAmount -= newProductList[index].price;
+    }
     setTotalAmount(newTotalAmount);
     setProductList(newProductList);
-  }
+  };
 
-  const reset=()=>{
-    let newProductList=[...productList];
-    newProductList.map((products)=>{products.quantity=0})
+  const reset = () => {
+    let newProductList = [...productList];
+    newProductList.map((products) => {
+      products.quantity = 0;
+    });
     setProductList(newProductList);
     setTotalAmount(0);
-  }
+  };
 
+  const remove = (index) => {
+    let newProductList = [...productList];
+    let newTotalAmount = totalAmount;
+    newTotalAmount -=
+      newProductList[index].quantity * newProductList[index].price;
+    newProductList.splice(index, 1);
+    setTotalAmount(newTotalAmount);
+    setProductList(newProductList);
+  };
 
   return (
     <>
-        <Navbar/>
-        <main className='container mt-5'>
-        <ProductList productList={productList} incrementQuantity={incrementQuantity} decrementQuantity={decrementQuantity}/> 
-        </main>
-        <Footer totalAmount={totalAmount} reset={reset}/>
-    
+      <Navbar />
+      <main className="container mt-5">
+        <ProductList
+          productList={productList}
+          incrementQuantity={incrementQuantity}
+          decrementQuantity={decrementQuantity}
+          remove={remove}
+        />
+      </main>
+      <Footer totalAmount={totalAmount} reset={reset} />
     </>
   );
 }
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -3,8 +3,9 @@ import Product from './Product.js'
 
 export default function ProductList(props) {
   return (
+    props.productList.length >0 ?
     props.productList.map((product,i)=>{
-        return <Product product={product} key={i} incrementQuantity={props.incrementQuantity} decrementQuantity={props.decrementQuantity} index={i}/>
-    })
+        return <Product product={product} key={i} incrementQuantity={props.incrementQuantity} decrementQuantity={props.decrementQuantity} index={i} remove={props.remove}/>
+    }): <h1>No Products in Your Cart :( </h1>
   )
 }
